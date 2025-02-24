938d9e93178340b6a10cf8704e82d3b1416762af
杨熙伟 <yangxiwei@yangxiweideMacBook-Pro.local>
2025-02-24 14:00:03 -0500

    初次提交

---
+++ React-course 01/27_react-app/27_ 使用create-react-app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode> 
+    <App/>
+  </React.StrictMode>
+);
+//使用严格的要求来渲染的模式
+
+
+
+
+// npm create-react-app   npx
